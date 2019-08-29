47c076b2a555af88073034202666880b0a1699c8
gustavo <gusbpb@gmail.com>
2019-08-28 22:48:12 -0300

    master(feat): initial commit

---
+++ App.js
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Fragment} from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Gustavo2</Text><Text>Gustavo3</Text>
+    </View>
+  );
+};
+
+export default App;
